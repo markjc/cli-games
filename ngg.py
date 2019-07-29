e317b7d1c650fab6d44e166b1a20c42e9a75a3f2
Mark Crabtree <markjc@live.com>
2019-07-28 23:48:56 -0400

    Current NGG update, Added Difficulty Selection, Adjusted Main Game Loop, progressing Through Debug

--- ngg.py
+++ ngg.py
@@ -29,7 +29,7 @@ class Game:
         self.initialRun = True
         self.WINS = 0
         self.LOSSES = 0
-        self.difficulty = {10:"1", 50:"2", 100:"3", 500:"4", 1000:"5", 1000000:"6"}
+        self.difficulty = {"1": 10, "2": 50, "3": 100, "4": 500, "5": 1000, "6": 1000000}
         
 
 ###########################################
@@ -40,17 +40,17 @@ class Game:
     def intro(self):
         ### Clear the terminal and display the Game Title and Intro ###
         printer = printerObj.TPrinter()
-        os.system('clear')
+        os.system('cls' if os.name == 'nt' else 'clear')
         print('*************************************************')
         print("*                  Let's Play:                  *")
         print('*************************************************')
         
         ### If this is the First play, Implement Title Animation ###
-        if self.firstPlay == True:
+        if self.firstPlay:
             printer.tprint('              '+self.gameTitle+'             \n',0.02)
         else:
             print("              "+self.gameTitle+"              ")
-        printer.tprint("          Wins: " + str(self.WINS) + " - Losses: " + str(self.LOSSES) + " - Ties: " + str(self.TIES),0.01)
+        #printer.tprint("          Wins: " + str(self.WINS) + " - Losses: " + str(self.LOSSES) + " - Ties: " + str(self.TIES),0.01)
         print()
 
 ###########################################
@@ -69,10 +69,10 @@ class Game:
         print("5. Very Hard [1-1000]")
         print("6. Super Epic Crazy Hard [1-1,000,000]")
         userInput = input("--> ")
-        while userInput not in self.difficulty:
+        while int(userInput) not in range(1, 6):
             print("Please enter a valid choice: 1-6")
             userInput = input("--> ")
-        return self.difficulty[userInput]
+        return self.difficulty[int(userInput)]
         
 
 ###########################################
@@ -82,7 +82,8 @@ class Game:
 ########################################### 
        
     def gameLoop(self):
-        ###define options for input and computer selection, create conversational response object, and alter firstPlay boolean
+
+        ### create conversational response object, and alter firstPlay boolean
         
         speak = convo.Convo()
         self.firstPlay = False
@@ -92,34 +93,39 @@ class Game:
             #Play the Game.
             playAgain = ''
             #### If it's the first play, greet the user, else continue the game.
-            if self.firstPlay == True:
+            if self.firstPlay:
                 print(speak.greetings[random.randint(0, len(speak.greetings)-1)])
             sleep(1)
             
             #__________________________________________________________
             
-            
+            userGuess = 0
+            targetNum = random.randint(1, limit)
+
+            while userGuess != targetNum:
+                print("Please guess a number between 1 and " + str(limit))
+                userGuess = int(input("--> "))
+
+                if userGuess < targetNum:
+                    print("Nope, The number is Higher.")
+                elif userGuess > targetNum:
+                    print("Nope, The number is Lower.")
+                else:
+                    print("Congratulations! You Guessed it!")
+                    self.initialRun = False
+                    break
+
             
             
             #__________________________________________________________
-            
-            #Determine if a win condition has been reached
-            result = self.checkWin(selection, choice)
-            
-            #Communicate the win condition to the user, and increment the appropriate counter for score generation
-            if result == 'win':
-                print(speak.wins[random.randint(0, len(speak.wins)-1)])
-                self.WINS +=1
-            elif result == 'loss':
-                print(speak.losses[random.randint(0, len(speak.losses)-1)])
-                self.LOSSES +=1
+
             
             
             #Determine if the user wants to play again, or navigate back to the main menu
             print()
             playAgain = input('Would you like to play again? -- y for yes, or n for no -->')
             if playAgain == 'y':
-                os.system('clear')
+                os.system('cls' if os.name == 'nt' else 'clear')
                 self.intro()
                 continue
             else:
